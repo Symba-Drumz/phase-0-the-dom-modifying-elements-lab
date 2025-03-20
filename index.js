be59b335f33133bb2676f385de1a42233ad4635d
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-03-20 08:17:36 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainEl = document.querySelector('main#main')
+mainEl.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+document.body.appendChild(newHeader)
